6bc273e19bebe005e00d273a068e39a0f725fcdb
ammaroker-x <oker.ammar@gmail.com>
2024-06-30 14:53:32 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Card, Text } from '@mantine/core';
+
+export default function App() {
+  return (
+    <Card withBorder shadow="lg" m={8}>
+      <Text>Hello, Mantine!</Text>
+    </Card>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import '@mantine/core/styles.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MantineProvider } from '@mantine/core';
+import App from './App.tsx';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>,
+);
